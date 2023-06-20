017992e379d762b8e4639c6393d0dd02c59b87cd
Lavina <lavikalda@gmail.com>
2023-06-19 21:34:37 -0700

    g

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,13 @@ export default function Card(props) {
 
   // let options = props.options;
   //  let ooptions = Object.keys(options);
-  let foodItem = props.foodItems;
-      const handleAddToCart = ()=>{
-
-      }
+  
   return (
     <div>
        <div><div className="card mt-3" style={{"width": "18rem", "maxHeight":"360px"}}>
-  <img src={props.foodItems.img} className="card-img-top" alt="..." style={{height:"150px",objectFit:"film"}}/>
+  <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"150px",objectFit:"film"}}/>
   <div className="card-body">
-    <h5 className="card-title">{props.foodItem.name}</h5>
+    <h5 className="card-title">{props.foodName}</h5>
     
     <div className='container w-100' >
         <select className='m-2 h-100  bg-secondary rounded'>
@@ -37,7 +34,7 @@ export default function Card(props) {
         </div>
     </div>
     <hr></hr>
-    <button className={'btn btn-success justify-center ms-2'} onClick={handleAddToCart}>Add to Cart</button>
+    <button className={'btn btn-success justify-center ms-2'} >Add to Cart</button>
   </div>
 </div></div>
     </div>
